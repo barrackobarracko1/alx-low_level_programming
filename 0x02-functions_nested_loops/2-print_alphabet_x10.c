20f626df8a6e0f52287ab5c29976fd20c19bc01b
Stephen Barracks Asemiah <barrackobarracko1@gmail.com>
2023-10-11 16:23:38 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,6 +2,7 @@
 
 /**
  * print_alphabet_x10 - make alphabet x10 times
+ *
  * Return: void
  */
 
@@ -11,7 +12,6 @@ void print_alphabet_x10(void)
 	char ch;
 	int i;
 	i = 0;
-
 	while (i < 10)
 	{
 	for (ch = 'a' ; ch <= 'z' ; ch++)
