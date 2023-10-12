c90382c19dc908f8b8490cd44e4728c4b3444e8d
Stephen Barracks Asemiah <barrackobarracko1@gmail.com>
2023-10-11 17:16:31 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,12 +10,14 @@ void print_alphabet_x10(void)
 
 {
 	char ch;
-	int i;
-	i = 0;
-	while (i < 10)
+	int i = 0;
+
+	while (i <= 9)
 	{
 	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
 	_putchar(ch);
+	}
 	_putchar('\n');
 	i++;
 	}
