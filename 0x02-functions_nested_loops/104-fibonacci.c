72269183b55c3ae6e68a3dde7744952e3e3cf9a8
Stephen Barracks Asemiah <barrackobarracko1@gmail.com>
2023-10-11 21:49:11 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -12,9 +12,9 @@ int main(void)
 	int count;
 	unsigned long fib1 = 0, fib2 = 1, sum;
 	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
-	unsigned long half1, half2
+	unsigned long half1, half2;
 
-	for (count = 0; count < 92; count++)
+	for (count = 0; count < 92; count++;)
 	{
 	sum = fib1 + fib2;
 	printf("%lu, ", sum);
@@ -28,7 +28,7 @@ int main(void)
 	for (count = 93; count < 99; count++)
 	{
 	half1 = fib1_half1 + fib2_half1;
-        half2 = fib1_half2 + fib2_half2;
+        half1 = fib1_half2 + fib2_half2;
         if (fib1_half2 + fib2_half2 > 9999999999)
 	{
 	half1 += 1;
