f935a45c77a4807f2a50da7231de643606304587
Stephen Barracks Asemiah <barrackobarracko1@gmail.com>
2023-10-19 09:46:49 -0700

    tough

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * string_toupper - function that changes all lowercase letters of a string to uppercase
+ * string_toupper - change all the lowercase to uppercase
  * @n: pointer
  * Return: n
  */
 
-char *string_toupper(char *)
+char *string_toupper(char *n)
 {
-	int r;
+	int h;
 
-	r = 0;
-	while (n[r] != '\0')
+	h = 0;
+	while (n[h] != '\0')
 	{
-	if (n[r] >= 'a' && n[r] <= 'z')
-	n[r] = n[r] -32;
-	r++;
+		if (n[h] >= 'a' && n[h] <= 'z')
+			n[h] = n[h] - 32;
+		h++;
 	}
 	return (n);
 }
