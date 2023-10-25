e554441257f9737ff95f9b57fd751ad2d96f1918
Stephen Barracks Asemiah <barrackobarracko1@gmail.com>
2023-10-24 20:00:43 -0700

    C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -11,9 +11,9 @@ void print_chessboard(char (*a)[8])
 	int v;
 	int g;
 
-	for (v = 0; v < 0; v++)
+	for (v = 0; v < 8; v++)
 	{
-	for (g = 0; g < 0; g++)
+	for (g = 0; g < 8; g++)
 	_putchar(a[v][g]);
 	_putchar('\n');
 	}
